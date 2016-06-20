ae6769ff8668770e1b44912725e87dc385a2b14f
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-06-20 15:56:53 -0700

    added files

---
+++ js-exercises/javascript-intro.js
@@ -0,0 +1,42 @@
+// JavaScript Intro
+
+// 1.  Given the following object, log the name of the student with id 3.
+
+var students = [
+  {
+  id: 1,
+    name: 'Aaron',
+    row: 1,
+    usesMac: true
+  },
+  {
+    id: 2,
+    name: 'Mike',
+    row: 2,
+    usesMac: false
+  },
+  {
+    id: 3,
+    name: 'Markham',
+    row: 3,
+    usesMac: true
+  },
+  {
+    id: 4,
+    name: 'Blaze',
+    row: 2,
+    usesMac: true
+  }
+]
+
+// 2.  Write a function to add students to the list of students, then print the
+//     new student object from the object.  Execute the function for 3 new students.
+//  Hint:  You add to an array like this:  myArray.push(object)
+//  Hint:  You can get the number of objects in an array by using myArray.length
+
+
+// 3.  Write a function that adds 5 to a number, then multiplies the result by 50.
+//     Log the result.
+
+
+// 4.  Show an example of function scope by logging variables in different scopes.

822aaa78357ec8a0ae82e00e0311c93f7952e087
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-06-21 16:36:54 -0700

    added more js to cars

--- js-exercises/car-project/garage.js
+++ js-exercises/car-project/garage.js
@@ -28,18 +28,46 @@ var garage = {
   ]
 };
 
-var createCar = function() {
+garage.createCar = function() {
   var make = prompt('What is the make of the car?');
   var model = prompt('What is the model?');
   var color = prompt('What color is it?');
-  garage.cars.push({make: make, model: model, color: color});
+  this.cars.push({make: make, model: model, color: color});
 };
 
-var searchCar = function() {
+garage.searchCar = function() {
   var pickCar = prompt('Pick a car model!');
-    for (var i = 0; i < garage.cars.length; i++) {
-      if (pickCar === garage.cars[i].model) {
-        return garage.cars[i]
+    for (var i = 0; i < this.cars.length; i++) {
+      if (pickCar === this.cars[i].model) {
+        return this.cars[i]
       }
     }
+    console.log('Car model not found')
+};
+
+var Garage = function(cars) {
+  this.cars = cars;
+};
+
+Garage.prototype.getCars = function() {
+  return this.cars;
+};
+
+
+var Car = function(make, model, color) {
+  this.make = make;
+  this.model = model;
+  this.color = color;
 }
+
+Car.prototype.getMake = function () {
+  return this.make;
+};
+
+Car.prototype.getModel = function () {
+  return this.model;
+};
+
+Car.prototype.getColor = function () {
+  return this.color;
+};

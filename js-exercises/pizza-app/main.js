91a4326b8d83b7418e6352c5be330b275fc689a7
Aaron Martin <grav3serker@Aarons-MacBook-Pro.local>
2016-06-27 09:59:45 -0700

    adding final code

--- js-exercises/pizza-app/main.js
+++ js-exercises/pizza-app/main.js
@@ -1,79 +1,123 @@
 /*
 GOALS FOR PIZZA UNICORN
-1.  PU has Customers
-      - Customers can Sign Up
-      - Customers create Orders
 2.  Orders contain one or many Pizzas
 3.  Pizzas have one or many Items
 4.  PU has many Delivery People
 5.  Orders get assigned to a Random Delivery Person
 */
 
-//1.  PU has Customers
-//      - Customers can Sign Up
-//      - Customers create Orders
+// *********** Initialize ***********
 
-var pageAddCustomer = document.getElementById('page-add-customer')
-var pageAddPizza = document.getElementById('page-add-pizza')
-var pageCart = document.getElementById('page-cart')
-var pageReceipt = document.getElementById('page-receipt')
-var navLinks = document.querySelectorAll('.nav-link');
+var pageAddCustomer = document.getElementById('page-add-customer');
+var pageAddPizza = document.getElementById('page-add-pizza');
+var pageCart = document.getElementById('page-cart');
+var pageReceipt = document.getElementById('page-receipt');
 var buttonCustomer = document.getElementById('btn-add-customer');
 var buttonPizza = document.getElementById('btn-add-pizza');
 var buttonCart = document.getElementById('btn-add-cart');
+var linkAddMorePizzas = document.getElementById('add-more-pizzas');
 
-var firstName = document.getElementById('firstName')
-var lastName = document.getElementById('lastName')
-var email = document.getElementById('email')
-var state = document.getElementById('state')
-var zip = document.getElementById('zip')
-
-var customers = [];
+var firstName = document.getElementById('firstName');
+var lastName = document.getElementById('lastName');
+var email = document.getElementById('email');
+var state = document.getElementById('state');
+var zip = document.getElementById('zip');
+var pizzaDropdown = document.getElementById('pizza-dropdown');
+var pizzaSizeList = document.getElementById('pizza-size-list');
+var navLinks = document.querySelectorAll('.nav-link');
 
 var customer = {};
 var order = {};
 var pizza = {};
 
-// Page 1
+// *********** Create Customer Page ***********
+pageAddCustomer.style.display = 'block';
+var currentPage = pageAddCustomer;
+
 buttonCustomer.addEventListener( 'click', function() {
   customer = new Customer(firstName.value, lastName.value, email.value, state.value, zip.value);
-  customers.push(customer);
-  var order = new Order(customer);
+  order = new Order(customer);
+
+  loadPizzaOptions();
+
+  console.log('');
+  console.log('------ Customer Order Created! ------');
+  console.log(customer);
+  console.log(order);
 
   navigate(currentPage, pageAddPizza);
 });
 
-//Page 2
+// *********** Add Pizzas Page ***********
 buttonPizza.addEventListener( 'click', function() {
   pizza = new Pizza();
-  var pizzaSize = new PizzaSize('name', 'cost');
+  var pizzaSize = new PizzaSize(pizzaDropdown.dataset.name, pizzaDropdown.dataset.cost);
   pizza.setSize(pizzaSize);
+  order.addPizza(pizza);
 
-  customers.push(customer);
+  console.log('');
+  console.log('------ Pizza Added! ------');
+  console.log(pizza);
+  console.log(order.pizzas);
+
   navigate(currentPage, pageCart);
 });
 
-//Page 3
+var loadPizzaOptions = function() {
+  var caretText = ' <span class="caret"></span>';
+  pizzaDropdown.innerHTML = "Select a Pizza" + caretText;
+  delete pizzaDropdown.dataset.name;
+  delete pizzaDropdown.dataset.cost;
+  pizzaSizeList.innerHTML = '';
+
+
+  for (i = 0; i < allPizzaSizes.length; i++) {
+    var listItemLink = document.createElement('a');
+    listItemLink.href = '#';
+    listItemLink.dataset.name = allPizzaSizes[i].name;
+    listItemLink.dataset.cost = allPizzaSizes[i].cost;
+    listItemLink.innerHTML = allPizzaSizes[i].name;
+
+    listItemLink.addEventListener( 'click', function() {
+      pizzaDropdown.innerHTML = this.innerHTML + caretText;
+      pizzaDropdown.dataset.name = this.dataset.name;
+      pizzaDropdown.dataset.cost = this.dataset.cost;
+    });
+
+    var listItem = document.createElement('li');
+    listItem.appendChild(listItemLink);
+    pizzaSizeList.appendChild(listItem);
+  }
+
+}
+
+// *********** Show Cart Page ***********
 buttonCart.addEventListener( 'click', function() {
   var deliveryPerson = deliveryPeople[ Math.floor(Math.random() * deliveryPeople.length) ];
   order.addDeliveryPerson(deliveryPerson);
 
+  console.log('');
+  console.log('------ Order Complete! ------');
   console.log(customer);
   console.log(order);
-  console.log(pizza);
-  navigate(currentPage, pageReceipt);
 
+  navigate(currentPage, pageReceipt);
 });
 
-pageAddCustomer.style.display = 'block';
-var currentPage = pageAddCustomer;
+linkAddMorePizzas.addEventListener( 'click', function() {
+  loadPizzaOptions();
+  navigate(currentPage, pageAddPizza);
+});
 
+
+// *********** Navigation ***********
 var navigate = function(pageFrom, pageTo) {
   pageFrom.style.display = 'none';
   currentPage = pageTo;
   currentPage.style.display = 'block';
 }
 
+// Nav links
 for (i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener( 'click', function() {
     switch (this.id) {
@@ -94,28 +138,26 @@ for (i = 0; i < navLinks.length; i++) {
         break;
 
       default:
-        console.log('You clicked a link that I don\'t know about! Hurry! Run back!');
-
+        console.log('404');
     }
   });
 }
 
+// *********** App Objects ***********
+
 var Customer = ( function() {
   var incrementedId = 1;
 
-  return function Customer(firstname, lastname, phone, email, address1, city, state, zip) {
+  return function Customer(firstName, lastName, email, state, zip) {
     this.id = incrementedId++,
-    this.firstname = firstname,
-    this.lastname = lastname,
-    this.phone = phone,
+    this.firstName = firstName,
+    this.lastName = lastName,
     this.email = email,
-    this.address1 = address1,
-    this.city = city,
     this.state = state,
     this.zip = zip
   }
 
-})();
+} )();
 
 var Order = ( function(customer) {
   var incrementedId = 10001;
@@ -124,81 +166,44 @@ var Order = ( function(customer) {
     this.id = incrementedId++,
     this.customerId = customer.id,
     this.deliveryPerson = {},
-    this.pizzas = []
+    this.pizzas = [],
     this.totalCost = 0,
-    this.addPizzas = function(pizza) {
+    this.addPizza = function(pizza) {
       this.pizzas.push(pizza);
-      this.totalCost = pizza.totalCost;
+      this.totalCost += pizza.totalCost;
     },
     this.addDeliveryPerson = function(deliveryPerson) {
       this.deliveryPerson = deliveryPerson;
     }
   }
-
 } )();
 
 var Pizza = function() {
   this.toppings = [],
-  this.totalCost = 0;
-  this.size = {};
+  this.totalCost = 0,
+  this.size = {},
   this.setSize = function(size) {
     this.size = size;
     this.totalCost += parseInt(size.cost)
   }
-  this.addTopping = function(topping) {
+  this.addTopping = function(topping){
     this.toppings.push(topping);
     this.totalCost += parseFloat(topping.cost);
   }
-};
+}
 
 var Topping = function(name, cost) {
   this.name = name,
   this.cost = cost
-};
-
-var allToppings = [
-  new Topping('Pepperoni', 0),
-  new Topping('Bacon', .50),
-  new Topping('Jalepeno', 0),
-  new Topping('Lobster', 3),
-  new Topping('Mac \'n Cheese', 2),
-  new Topping('Caviar', 10),
-]
+}
 
 var PizzaSize = function(name, cost) {
   this.name = name,
-  this.cost = cost
-};
-
-var allPizzaSizes = [
-  new PizzaSize('Small', 10),
-  new PizzaSize('Medium', 14),
-  new PizzaSize('Large', 20)
-]
-
-
-
-// Make a pizza in console!
-
-pizza = new Pizza();
-pizza.setSize(allPizzaSizes[1])
-pizza.addTopping(allToppings[1]);
-pizza.addTopping(allToppings[3]);
-pizza.addTopping(allToppings[4]);
-
-// End make pizza in console
+  this.cost = parseFloat(cost)
+}
 
 var DeliveryPerson = function(name, phone, car) {
   this.name = name,
   this.phone = phone,
   this.car = car
 }
-
-var deliveryPeople = [
-  new DeliveryPerson('Markus', '415-555-1212', 'Ferrari'),
-  new DeliveryPerson('Liam', '666-555-1212', 'Pinto'),
-  new DeliveryPerson('Markham', '948-444-1212', 'Bently'),
-  new DeliveryPerson('Blaze', '309-995-1212', 'Moped'),
-  new DeliveryPerson('Jayson', '818-995-1212', 'Hover Board'),
-  new DeliveryPerson('Markus', '233-555-1212', 'Ford Fiesta'),
-]
